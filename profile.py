b9292a84d8ab2171e0ebc73c9f3ffad0894ace33
JM825966 <43188989+JM825966@users.noreply.github.com>
2018-09-16 21:48:32 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,14 +16,24 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-node.routable_control_ip = "true"
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+link = request.LAN("lan")
+ 
+#Creating nodes using a for loop starting at 1 (as the node name should start with node-1 and the starting ip address ends with .1)
+for i in range(1,5):
+    node = request.XenVM(str("node-") + str(i))
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+    inter = node.addInterface("if" + str(i))
+    inter.component_id = "eth1"
+    inter.addAddress(pg.IPv4Address("192.168.1" + str(i), "255.255.255.0"))
+    link.addInterface(inter)
+    
+    if(i == 1):
+        node.routable_control_ip = "true"
+
+
+    # Install and execute a script that is contained in the repository.
+    node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
